de1684f87358c0138c5f1b35e7699659e5d099c7
rfgcode <r.baral@fg-branddesign.com>
2026-03-27 14:08:39 +1300

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,22 +1,21 @@
-import { NextResponse } from 'next/server';
-
-export function middleware(request) {
+export default function middleware(request) {
   const basicAuth = request.headers.get('authorization');
 
   if (basicAuth) {
     const [, credentials] = basicAuth.split(' ');
     const [user, password] = atob(credentials).split(':');
     if (user === 'admin' && password === process.env.SITE_PASSWORD) {
-      return NextResponse.next();
+      return new Response(null, { status: 200 });
     }
   }
 
-  return new NextResponse('Authentication required', {
+  return new Response('Authentication required', {
     status: 401,
     headers: { 'WWW-Authenticate': 'Basic realm="Secure Area"' },
   });
 }
 
 export const config = {
-  matcher: ['/((?!_next).*)'],
+  matcher: '/(.*)',
+  runtime: 'edge',
 };
